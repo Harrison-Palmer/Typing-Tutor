09dba27ba17525907c0ea3e88ee8759257bf0a3d
Ross O'Brien <rossobrien2250@gmail.com>
2015-03-27 14:48:06 -0400

    test
    making sure this works

--- Tutor.cpp
+++ Tutor.cpp
@@ -1,1 +1,10 @@
 #include "tutor.h"
+
+
+int typer::getLength(){
+	int input;
+
+	//input = System::int(textBox1.Text);
+
+	return input;
+}
--- Tutor.h
+++ Tutor.h
@@ -6,6 +6,7 @@ public ref class typer{
 private:
 
 public:
+	int getLength();
 
 
 };
