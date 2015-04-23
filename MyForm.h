86ba734100cfbb7bd64596fbb6a673871be83099
Harrison <hpalmer@student.dwc.edu>
2015-04-23 16:16:40 -0400

    Revert "loaded images"
    This reverts commit b22f2c93c7b69ab70c10b0059942c9d3f51986bb.
    
    Conflicts:
    	MyForm.h
    
    	modified:   Debug/CS113For.D9DE65A6.tlog/CL.read.1.tlog
    	modified:   Debug/CS113For.D9DE65A6.tlog/CL.write.1.tlog
    	modified:   Debug/CS113For.D9DE65A6.tlog/CS113FormsProject.lastbuildstate
    	modified:   Debug/CS113For.D9DE65A6.tlog/CS113FormsProject.write.1u.tlog
    	modified:   Debug/CS113For.D9DE65A6.tlog/cl.command.1.tlog
    	modified:   Debug/CS113For.D9DE65A6.tlog/link.command.1.tlog
    	modified:   Debug/CS113For.D9DE65A6.tlog/link.read.1.tlog
    	modified:   Debug/CS113For.D9DE65A6.tlog/link.write.1.tlog
    	modified:   Debug/CS113FormsProject.exe
    	modified:   Debug/CS113FormsProject.pdb
    	deleted:    Debug/MyForm.obj
    	modified:   Debug/Project1.log
    	modified:   Debug/Project1.vcxproj.GenerateResource.Cache
    	modified:   Debug/Tutor.obj
    	modified:   Debug/vc120.pdb
    	deleted:    Images/letter_file.bmp
    	modified:   MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -45,21 +45,27 @@ namespace Project1 {
 	private: System::Windows::Forms::Panel^  panel1;
 
 	private: System::Windows::Forms::PictureBox^  Second_Button;
+
 	private: System::Windows::Forms::PictureBox^  Start_Pictutre;
 
 	private: System::Windows::Forms::PictureBox^  MenuPicture;
 	private: System::Windows::Forms::PictureBox^  Exit_Button;
 	private: System::Windows::Forms::PictureBox^  PictureBoxTild;
 	private: System::Windows::Forms::PictureBox^  pictureBoxSpac;
+
 	private: System::Windows::Forms::PictureBox^  pictureBoxRAlt;
 	private: System::Windows::Forms::PictureBox^  pictureBoxRWin;
 	private: System::Windows::Forms::PictureBox^  pictureBoxFile;
 	private: System::Windows::Forms::PictureBox^  pictureBoxRCtr;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 
 >>>>>>> origin/master
+=======
+
+>>>>>>> parent of b22f2c9... loaded images
 	private: System::Windows::Forms::PictureBox^  pictureBoxLAlt;
 	private: System::Windows::Forms::PictureBox^  pictureBoxLWin;
 	private: System::Windows::Forms::PictureBox^  pictureBoxLCtr;
@@ -101,11 +107,14 @@ namespace Project1 {
 	private: System::Windows::Forms::PictureBox^  pictureBoxw;
 	private: System::Windows::Forms::PictureBox^  pictureBoxq;
 	private: System::Windows::Forms::PictureBox^  pictureBoxTAB;
+
 	private: System::Windows::Forms::PictureBox^  pictureBoxBS;
+
 	private: System::Windows::Forms::PictureBox^  pictureBoxPlus;
 <<<<<<< HEAD
 	private: System::Windows::Forms::PictureBox^  PictureBoxDash;
 	private: System::Windows::Forms::PictureBox^  pictureBoxZERO;
+<<<<<<< HEAD
 	private: System::Windows::Forms::PictureBox^  pictureBoxNINE;
 	private: System::Windows::Forms::PictureBox^  pictureBoxEIGH;
 	private: System::Windows::Forms::PictureBox^  pictureBoxSEVE;
@@ -119,6 +128,8 @@ namespace Project1 {
 =======
 private: System::Windows::Forms::PictureBox^  PictureBoxDash;
 private: System::Windows::Forms::PictureBox^  pictureBoxZERO;
+=======
+>>>>>>> parent of b22f2c9... loaded images
 
 
 private: System::Windows::Forms::PictureBox^  pictureBoxNINE;
@@ -139,9 +150,13 @@ private: System::Windows::Forms::PictureBox^  pictureBoxTHREE;
 private: System::Windows::Forms::PictureBox^  pictureBoxTWO;
 
 private: System::Windows::Forms::PictureBox^  pictureBoxONE;
+<<<<<<< HEAD
 >>>>>>> origin/master
+=======
+>>>>>>> parent of b22f2c9... loaded images
 
 	private: System::Windows::Forms::TextBox^  textBox1;
+private: System::Windows::Forms::PictureBox^  pictureBoxRShi;
 
 	private:
 		/// <summary>
@@ -1083,7 +1098,7 @@ private: System::Windows::Forms::PictureBox^  pictureBoxONE;
 		Image^ b = gcnew Bitmap("Images\\letter_b.bmp");
 		Image^ c = gcnew Bitmap("Images\\letter_c.bmp");
 		Image^ d = gcnew Bitmap("Images\\letter_d.bmp");
-		Image^ ee = gcnew Bitmap("Images\\letter_e.bmp");
+		Image^ e = gcnew Bitmap("Images\\letter_e.bmp");
 		Image^ f = gcnew Bitmap("Images\\letter_f.bmp");
 		Image^ gg = gcnew Bitmap("Images\\letter_g.bmp");
 		Image^ h = gcnew Bitmap("Images\\letter_h.bmp");
@@ -1112,27 +1127,37 @@ private: System::Windows::Forms::PictureBox^  pictureBoxONE;
 		Image^ TAB = gcnew Bitmap("Images\\tab_key1.bmp");
 		Image^ LWin = gcnew Bitmap("Images\\windows_key2.bmp");
 		Image^ RWin = gcnew Bitmap("Images\\windows_key2.bmp");
+<<<<<<< HEAD
 		Image^ File = gcnew Bitmap("Images\\letter_file.bmp");
 
 	//private: System::Windows::Forms::PictureBox^  MenuPicture;
 	//private: System::Windows::Forms::PictureBox^  Exit_Button;
 
 <<<<<<< HEAD
+=======
+		Image^ File = gcnew Bitmap("Images\\alt_key.bmp");
+>>>>>>> parent of b22f2c9... loaded images
 
 		array<char>^ Keys = gcnew array<char>(61);
 		array<Control^>^ Keysss = gcnew array<Control^>(61);
-		/*
+
+
 		enum Pictures {
 			Tild, ONE, TWO, THRE, FOUR, FIVE, SIX, SEVE, EIGH, NINE, ZERO, Dash, Plus,
-			BS, TAB, q, w, ee, r, t, y, u, i, o, p, LBra, RBra,
+			BS, TAB, q, w, e, r, t, y, u, i, o, p, LBra, RBra,
 			FSla, Cap, a, s, d, f, gg, h, j, k, l, Semi, Quot,
 			Ent, LShi, z, x, c, v, b, n, m, Comm, Dot, BSla,
 			RShi, LCtr, LWin, LAlt, Spac, RAlt, RWin, File, RCtr
 		};
+<<<<<<< HEAD
 		*/
 		//Pictures imgur;
 =======
 >>>>>>> origin/master
+=======
+
+		Pictures imgur;
+>>>>>>> parent of b22f2c9... loaded images
 		/* potential way of level design
 		char Level1[7];
 		char Level1[] = {'t', 'e', 's', 't', 'i', 'n', 'g'};
@@ -1142,68 +1167,6 @@ private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e
 			 g = panel1->CreateGraphics();
 			 Exit_Button->Image = ExitGame;
 			 Start_Pictutre->Image = StartGame;
-
-			 PictureBoxTild->Image = Tild;
-			 pictureBoxSpac->Image = Spac;
-			 pictureBoxRAlt->Image = RAlt;
-			 pictureBoxRWin->Image = RWin;
-			 pictureBoxFile->Image = File;
-			 pictureBoxRCtr->Image = RCtr;
-			 pictureBoxLAlt->Image = LAlt;
-			 pictureBoxLWin->Image = LWin;
-			 pictureBoxLCtr->Image = LCtr;
-			 pictureBoxBSla->Image = BSla;
-			 pictureBoxComm->Image = Comm;
-			 pictureBoxDot->Image = Dot;
-			 pictureBoxm->Image = m;
-			 pictureBoxn->Image = n;
-			 pictureBoxb->Image = b;
-			 pictureBoxv->Image = v;
-			 pictureBoxc->Image = c;
-			 pictureBoxx->Image = x;
-			 pictureBoxz->Image = z;
-			 pictureBoxLshi->Image = LShi;
-			 pictureBoxEnt->Image = Ent;
-			 pictureBoxQuot->Image = Quot;
-			 pictureBoxSemi->Image = Semi;
-			 pictureBoxl->Image = l;
-			 pictureBoxk->Image = k;
-			 pictureBoxj->Image = j;
-			 pictureBoxh->Image = h;
-			 pictureBoxgg->Image = gg;
-			 pictureBoxf->Image = f;
-			 pictureBoxd->Image = d;
-			 pictureBoxs->Image = s;
-			 pictureBoxa->Image = a;
-			 pictureBoxCap->Image = Cap;
-			 pictureBoxFSla->Image = FSla;
-			 pictureBoxRBra->Image = RBra;
-			 pictureBoxLBra->Image = LBra;
-			 pictureBoxp->Image = p;
-			 pictureBoxo->Image = o;
-			 pictureBoxi->Image = i;
-			 pictureBoxu->Image = u;
-			 pictureBoxy->Image = y;
-			 pictureBoxt->Image = t;
-			 pictureBoxr->Image = r;
-			 pictureBoxe->Image = ee;
-			 pictureBoxw->Image = w;
-			 pictureBoxq->Image = q;
-			 pictureBoxTAB->Image = TAB;
-			 pictureBoxBS->Image = ExitGame;
-			 pictureBoxPlus->Image = Plus;
-			 PictureBoxDash->Image = Dash;
-			 pictureBoxZERO->Image = ZERO;
-			 pictureBoxNINE->Image = NINE;
-			 pictureBoxEIGH->Image = EIGH;
-			 pictureBoxSEVE->Image = SEVE;
-			 pictureBoxSIX->Image = SIX;
-			 pictureBoxFIVE->Image = FIVE;
-			 pictureBoxFOUR->Image = FOUR;
-			 pictureBoxTHREE->Image = THRE;
-			 pictureBoxTWO->Image = TWO;
-			 pictureBoxONE->Image = ONE;
-			 pictureBoxRShi->Image = RShi;
 }
 private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
 }
@@ -1232,10 +1195,13 @@ private: System::Void Start_Pictutre_Click(System::Object^  sender, System::Even
 <<<<<<< HEAD
 				 */
 
+<<<<<<< HEAD
 			 /*
 =======
 		
 >>>>>>> origin/master
+=======
+>>>>>>> parent of b22f2c9... loaded images
 			//puts images in picture box
 			for (int i = 0; i < 61; i++)
 			{
@@ -1246,7 +1212,6 @@ private: System::Void Start_Pictutre_Click(System::Object^  sender, System::Even
 
 				//Controls->Find("pictureBox" + Keys[i], true)[0];// = Controls->Find("" + Keys[i], true)[0];
 			}
-			*/
 			/*
 			//changes picturebox visibility
 			for (int i = 0; i < 61; i++)
@@ -1254,10 +1219,6 @@ private: System::Void Start_Pictutre_Click(System::Object^  sender, System::Even
 				Controls->Find("pictureBox" + Keys[i], true)[0]->Visible = false;
 			}
 			*/
-
-			 
-
-
 }
 //dont know yet
 private: System::Void Second_Button_Click(System::Object^  sender, System::EventArgs^  e) {
